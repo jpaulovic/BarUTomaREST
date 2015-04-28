aa61f32d801abe6bf9affe96ab0be00d0d6fd1f0
Jakub Paulovic <j.s.paulovic@hotmai.com>
2015-04-28 16:14:54 +0200

    Added bar controller GET and POST methods for /bar/{id}/drink. Not tested.

---
+++ BarUTomaREST/Controllers/BarController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BarUTomaModels.Models;
+
+namespace BarUTomaREST.Controllers
+{
+    public class BarController : BaseController
+    {
+        [System.Web.Http.HttpGet]
+        [Route("/bar/{id}/drink")]
+        public ActionResult DrinkGet(int id)
+        {
+            Bar bar = BarRepository.FindByPK(id);
+            if (bar == null)
+            {
+                return new HttpStatusCodeResult(400);
+            }
+            List<DrinkBar> drinkBars = DrinkRepository.ListAllDrinksOnBar(bar);
+            return new JsonResult { Data = drinkBars };
+        }
+
+        [System.Web.Http.HttpPost]
+        [Route("/bar/{id}/drink")]
+        public ActionResult DrinkPost(int id, Drink drink, string info, Quantity price)
+        {
+            Bar bar = BarRepository.FindByPK(id);
+            if (bar == null)
+            {
+                return new HttpStatusCodeResult(400, "System cannot find the specified bar.");
+            }
+            DrinkBar drinkToAdd = new DrinkBar() {Drink = drink, Bar = bar, Info = info, Price = price};
+            if (bar.DrinksOnBar.Contains(drinkToAdd))
+            {
+                return new HttpStatusCodeResult(400, "Drink already exists on this bar!");
+            }
+            if (!bar.Drinks.Contains(drink))
+            {
+                bar.Drinks.Add(drink);
+            }
+            bar.DrinksOnBar.Add(drinkToAdd);
+            return new HttpStatusCodeResult(200);
+        }
+    }
+}
