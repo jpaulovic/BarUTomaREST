dbb80392b2d90819d0ffdbefd685115237dc50ba
Jakub Paulovic <j.s.paulovic@hotmail.com>
2015-05-30 21:32:51 +0200

    added ingredient/{id}

---
+++ BarUTomaREST/Controllers/IngredientController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using BarUTomaModels.Models;
+
+namespace BarUTomaREST.Controllers
+{
+    [System.Web.Http.Authorize]
+    public class IngredientController : BaseController
+    {
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("ingredient/{id}")]
+        public ActionResult GetIngredient(int id)
+        {
+            Ingredient ingredient = IngredientRepository.FindByPK(id);
+            if (ingredient == null)
+            {
+                return new HttpStatusCodeResult(404, "System cannot find the specified ingredient.");
+            }
+            return new JsonResult(){Data = ingredient};
+        }
+    }
+}
