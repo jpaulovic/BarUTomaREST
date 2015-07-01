532d6b6b4359a76b65128d33e65ba25b34cbc524
Jakub Paulovic <j.s.paulovic@hotmail.com>
2015-07-01 22:00:00 +0200

    Removed empty BarUTomaREST.Tests project

---
+++ BarUTomaREST/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Mvc;
+
+namespace BarUTomaREST.Controllers
+{
+    [System.Web.Http.Authorize]
+    public class UserController : BaseController
+    {
+        /// <summary>
+        /// Get information about currently logged user.
+        /// </summary>
+        /// <returns>Information about currently logged user (only for self). (JSON)</returns>
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("user/")]
+        public ActionResult GetUser()
+        {
+            return new JsonResult(){ Data = LoggedUser };
+        }
+    }
+}
